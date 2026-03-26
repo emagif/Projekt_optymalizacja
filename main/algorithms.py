d198388b8139e071661a8b8979b06ac194a77716
Emanuel <s194407@student.pg.edu.pl>
2026-03-26 21:39:38 +0100

    Dodano plik z algorytmami

---
+++ main/algorithms.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy.optimize import line_search
+
+def grad_rosenbrock(x1, x2)
+    grad_x1 = 
+    grad_x2 = 
+    return grad_x1, grad_x2
+
+    
+def Quasi_Newton_BFGS(start_x1, start_x2, funkcja_celu):
+    
+
+
+    xk = np.array([start_x1, start_x2])
+    Bk = np.eye(2)
+    
+
+    if(funkcja_celu ==1):
+        
+
+
+
+    elif(funkcja_celu == 2):
+
+
+    elif(funkcja_celu == 3):
+
