342821868f69dd6258a65668a974e65dc2ec652e
Emanuel <s194407@student.pg.edu.pl>
2026-03-29 18:38:37 +0200

    Dodane możliwości optymalizacji dwóch kolejnych funkcji

--- main/algorithms.py
+++ main/algorithms.py
@@ -10,7 +10,7 @@ def three_hump_camel_f(xk):
 def himmelblau_f(xk):
     return (xk[0]**2 + xk[1] - 11)**2 + (xk[0] + xk[1]**2 - 7)**2
 
-def grad_rosenbrock(xk): # liczony gradient dla Rosenbrocka
+def grad_rosenbrock(xk): #  gradient liczony dla Rosenbrocka
     grad_x1 = -2*(1 - xk[0]) - 400*xk[0]*(xk[1] - xk[0]**2)
     grad_x2 = 200*(xk[1] - xk[0]**2)
     return np.array([grad_x1, grad_x2])
@@ -21,7 +21,7 @@ def grad_three_hump_camel(xk): # liczony gradient dla wielbłądowej
     grad_x2 = xk[0] + 2 * xk[1]
     return np.array([grad_x1, grad_x2])
 
-def grad_himmelblau(xk): # liczony gradient dla f himmelblaua
+def grad_himmelblau(xk): 
     grad_x1 = 4 * xk[0] * ((xk[0]**2) + xk[1] - 11) + 2 * (xk[0] + (xk[1]**2) - 7)
     grad_x2 = 4 * xk[1] * (xk[0] + (xk[1]**2)-7) + 2 * ((xk[0]**2) + xk[1] - 11)
     return np.array([grad_x1, grad_x2])
@@ -77,7 +77,83 @@ def Quasi_Newton_BFGS(start_x1, start_x2, function):
             if np.any(np.isnan(xk)) or np.any(np.isinf(xk)):
                 break
 
-    return best_result, xk_1[0], xk_1[1], i
+        return best_result, xk_1[0], xk_1[1], i
+
+    elif(function == 2):
+        
+        grad_k = grad_three_hump_camel(xk)
+
+        while np.linalg.norm(grad_k) >= tol and max_iter >= i:
+            
+            val_k = three_hump_camel_f(xk)
+            # pk = np.linalg.solve(Hk, -grad_k)
+            pk = -Hk @ grad_k
+            alpha_k, fc, gc, f1, f2, f3 = line_search(three_hump_camel_f, grad_three_hump_camel, xk, pk) 
+
+            if(alpha_k is None or alpha_k > alpha_max):
+                alpha_k = alpha_max
+
+            print(f"alpha_k: {alpha_k}")
+            
+            xk_1 = xk + alpha_k * pk
+            val_k_1 = three_hump_camel_f(xk_1)
+            grad_k_1 = grad_three_hump_camel(xk_1)
+            sk = xk_1 - xk 
+            yk = grad_k_1 - grad_k
+            rho_k = 1.0 / (yk @ sk + 1e-12) # algebra siedzi, nawet jak yk nie jest transponowane (sprawdzone) - można unit test napisać; ta dodana wartość jest po to, żeby mianownik się nie wyzerował
+            Hk_1 = (np.eye(2) - rho_k * np.outer(sk,yk)) @ Hk @(np.eye(2) - rho_k * np.outer(yk, sk)) + rho_k * np.outer(sk, sk) # tutaj np.outer(x,y) to iloczyn zewnętrzny także matma się zgadza
+
+            differ = val_k_1 - val_k
+            xk = xk_1
+            Hk = Hk_1
+
+            val_k = val_k_1
+            i+=1
+            best_result = val_k_1
+            grad_k = grad_k_1
+
+            if np.any(np.isnan(xk)) or np.any(np.isinf(xk)):
+                break
+
+        return best_result, xk_1[0], xk_1[1], i
+
+    elif(function == 3):
+        
+        grad_k = grad_himmelblau(xk)
+
+        while np.linalg.norm(grad_k) >= tol and max_iter >= i:
+            
+            val_k = himmelblau_f(xk)
+            # pk = np.linalg.solve(Hk, -grad_k)
+            pk = -Hk @ grad_k
+            alpha_k, fc, gc, f1, f2, f3 = line_search(himmelblau_f, grad_himmelblau, xk, pk) 
+
+            if(alpha_k is None or alpha_k > alpha_max):
+                alpha_k = alpha_max
+
+            print(f"alpha_k: {alpha_k}")
+            
+            xk_1 = xk + alpha_k * pk
+            val_k_1 = himmelblau_f(xk_1)
+            grad_k_1 = grad_himmelblau(xk_1)
+            sk = xk_1 - xk 
+            yk = grad_k_1 - grad_k
+            rho_k = 1.0 / (yk @ sk + 1e-12) # algebra siedzi, nawet jak yk nie jest transponowane (sprawdzone) - można unit test napisać; ta dodana wartość jest po to, żeby mianownik się nie wyzerował
+            Hk_1 = (np.eye(2) - rho_k * np.outer(sk,yk)) @ Hk @(np.eye(2) - rho_k * np.outer(yk, sk)) + rho_k * np.outer(sk, sk) # tutaj np.outer(x,y) to iloczyn zewnętrzny także matma się zgadza
+
+            differ = val_k_1 - val_k
+            xk = xk_1
+            Hk = Hk_1
+            
+            val_k = val_k_1
+            i+=1
+            best_result = val_k_1
+            grad_k = grad_k_1
+
+            if np.any(np.isnan(xk)) or np.any(np.isinf(xk)):
+                break
+
+        return best_result, xk_1[0], xk_1[1], i
 
 
 
