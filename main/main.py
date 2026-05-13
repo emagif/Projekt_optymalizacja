dfbfd712aa0904c989d557c6d09a2eb6294b3695
jcbhin <jcbhinca@wp.pl>
2026-05-13 21:52:02 +0200

    Update main.py

--- main/main.py
+++ main/main.py
@@ -7,13 +7,6 @@ from algorithms import *
 if __name__ == "__main__":
     
 # 0.9 0.8
-    val, x1, x2, iter_num = Quasi_Newton_DFP(-2, -4, himmelblau_f, grad_himmelblau, himmelblau_f_draw_contour, himmelblau_f_draw_3D_surf, [-6, 6], [-6, 6])
+
+    val, x1, x2, iter_num = Quasi_Newton_DFP(0.9, 0.8, himmelblau_f, grad_himmelblau, himmelblau_f_draw_contour, himmelblau_f_draw_3D_surf, [-6, 6], [-6, 6])
     print(f"val: {val}, x1: {x1}, x2: {x2}, iter_num: {iter_num}")
-
-
-
-    
-    
-    
-    
-
